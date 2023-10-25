35ce96548e2207c54e6db7e3d7215b0f14b9a286
Andriatsiory Hasiniaina <andriatsiory.hasiniaina.ext@up.coop>
2023-10-25 09:21:32 +0200

    update script

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # script-az-mysql-flexible-backup-restore-all-database
+## MySQL Flexible Server Restoration Script
+# Automated MySQL Flexible Server Restoration
+
+## Introduction
+
+This guide presents a PowerShell script that automates the restoration of an Azure MySQL Flexible Server database from a production environment to a pre-production environment.
+You can run this script on Azure devops pipelines
+
+### Prerequisites
+
+Before using this script, ensure you have the Azure CLI installed and logged in. 
+
+### Variables
+
+Make sure to update the following pipeline variables in your Azure Pipelines environment or provide values directly in the script:
+
+- `SourceMysqlFlexibleName`: The name of the MySQL Flexible Server instance to restore from.
+- `DestMysqlFlexibleName`: The desired name of the restored MySQL Flexible Server instance.
+- `SourceResourcegroupName`: The name of the resource group where the source MySQL Flexible Server is located.
+- `DestResourcegroupName`: The name of the resource group where the restored MySQL Flexible Server will be created.
+- `MysqlAdminPassword`: The password for the admin user of the MySQL Flexible Server. You can update the name of the secret if the password is stored in Azure Key Vault.
+
+### Execution 
+
+1. Clone this repository to your local environment.
+2. Navigate to the script directory.
+3. Run the script using the Azure CLI (ensure you are logged in).
+4. Follow the prompts to enter the required details.
+
+### Script Workflow
+
+1. The script checks if the destination MySQL Flexible Server exists. If it does, the script proceeds with the restoration process. Otherwise, it displays an error message.
+2. The script retrieves the timestamp of the last backup from the source MySQL Flexible Server.
+3. If the destination MySQL Flexible Server exists, it is deleted.
+4. The script performs the restoration of the source MySQL Flexible Server to the destination MySQL Flexible Server using the specified backup timestamp.
+5. The admin password of the destination MySQL Flexible Server is updated.
+6. The script completes successfully.
+
+### Outputs
+
+- The script provides feedback on the execution, including success messages and error messages if applicable.
--- run.ps1
+++ run.ps1
@@ -1,24 +1,40 @@
-###UPDATE VARIABLE VALUES ACCORDING TO YOUR SETUP
-$SubscriptionName=""
-$SourceMysqlFlexible=""
-$DestMysqlFlexible=""
-$SourceResourcegroup=""
-$DestResourcegroup=""
-$KeyVaultName=""
-$AdminMySqlSecretName=""
+# ### UPDATE NAME AND VALUE IN PIPELINE VARIABLES OR LOCALS VALUES
+# SourceMysqlFlexibleName
+# DestMysqlFlexibleName
+# SourceResourcegroupName
+# DestResourcegroupName
+# MysqlAdminPassword #Update name secret if from keyvault
 
-az login
-az account set --subscription $SubscriptionName
+function Get-LastBackupTime ($ResourceGroup,$ServerName){
+    $backupTime = az mysql flexible-server backup list -g $ResourceGroup -n $ServerName --query "[?backupType=='FULL'].{Time:completedTime} | [0]" --output tsv
+    return $backupTime
+}
+Write-Host "Source : $(SourceResourcegroupName) , $(SourceMysqlFlexibleName)"
+Write-Host "Destination : $(DestResourcegroupName) , $(DestMysqlFlexibleName)"
+$serverExists = az mysql flexible-server show --name $(DestMysqlFlexibleName) --resource-group $(DestResourcegroupName) --query "name" --output tsv --only-show-errors
 
-$Timelastbackup = az mysql flexible-server backup list -g $SourceResourcegroup -n $SourceMysqlFlexible  --query "[?backupType=='FULL'].{Time:completedTime} | [0]" --output tsv
-Write-Host "$Timelastbackup" 
-# Check server if exists
-az mysql flexible-server show --name $DestMysqlFlexible --resource-group $DestResourcegroup --query "name" --output tsv --only-show-errors
-# Get Secret value
-$secret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $AdminMySqlSecretName -AsPlainText
-# Delete flexible server Destination
-az mysql flexible-server delete -n $DestMysqlFlexible -g $DestResourcegroup --yes
-# Restore
-az mysql flexible-server restore -n $DestMysqlFlexible -g $DestResourcegroup --restore-time $Timelastbackup --source-server $SourceMysqlFlexible
-#Update Admin password
-az mysql flexible-server update -g $DestResourcegroup -n $DestMysqlFlexible --admin-password $secret
+if ($serverExists) {
+    Write-Host "The destination MySQL flexible server exists. Attempt to delete... Run restore script and update administrator password."
+    # GET last time backup
+    $Timelastbackup = Get-LastBackupTime -ResourceGroup $(SourceResourcegroupName) -ServerName $(SourceMysqlFlexibleName)
+    Write-Host "Last backup was run at $Timelastbackup"
+    # Delete Dest Server
+    az mysql flexible-server delete -n $(DestMysqlFlexibleName) -g $(DestResourcegroupName) --yes
+    Write-Host "Delete successfully"
+    # Restore
+    Write-Host "Run restore ...."
+    Write-Host "Restore source-server $SourceMysqlFlexibleName backuptime $Timelastbackup to $DestMysqlFlexibleName at $SourceResourcegroupName "
+    Write-Host "az mysql flexible-server restore -n $DestMysqlFlexibleName -g $SourceResourcegroupName --restore-time $Timelastbackup --source-server $SourceMysqlFlexibleName"
+    az mysql flexible-server restore -n $DestMysqlFlexibleName -g $SourceResourcegroupName --restore-time $Timelastbackup --source-server $SourceMysqlFlexibleName
+    # If you have a problem on ZoneAvailbility, set directly a Zone, comment line above and run decomment this 
+    # az mysql flexible-server restore -n $DestMysqlFlexibleName -g $SourceResourcegroupName --restore-time $Timelastbackup --source-server $SourceMysqlFlexibleName  --zone 2
+    Write-Host "Restore successfully"
+
+    # Update Admin password
+    Write-Host "update admin password on new DB server ...."
+    az mysql flexible-server update -g $SourceResourcegroupName -n $DestMysqlFlexibleName --admin-password  $MysqlAdminBenchPassword
+    Write-Host "Successful updating Admin Password."
+
+} else {
+    Write-Host "The destination MySQL flexible server does not exist or an error has occurred"
+}
